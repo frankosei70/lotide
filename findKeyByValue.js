dda6e95016f4fc9aa28d9f461dc3b35c81b92bf2
frank <frankosei70@yahoo.com>
2023-03-04 08:47:39 -0800

    adding the findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+const findKeyByValue = function(obj, value) {
+  const key = Object.keys(obj).find(key => obj[key] === value);
+  return key !== undefined ? key : undefined;
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
