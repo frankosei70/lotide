6ac18553387699964bb5bf4f4f54d6d802d4d9e6
frank <frankosei70@yahoo.com>
2023-05-14 18:01:56 -0700

    final update

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const middle = require('./middle');
 const countOnly = require('./CountOnly');
 const countLetters = require('./countLetters');
 const flatten= require('./flatten');
+const eqArrays = require('./eqArrays');
+const map = require('./map');
+const findKeyByValue= require('./findKeyByValue');
+const findKey = require('./findKey');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqObjects = require('./eqObjects');
+const assertEquals= require('./assertEquals');
+const without = require('./without');
+
+
+
 
 
 
@@ -13,7 +24,15 @@ module.exports = {
   countOnly, 
   countLetters, 
   flatten, 
-  tail
+  tail,
+  eqArrays,
+  map,
+  findKeyByValue,
+  findKey,
+  assertObjectsEqual,
+  eqObjects,
+  assertEquals,
+  without,
 
 
 };
